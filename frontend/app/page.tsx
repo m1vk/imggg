ca412a6a6b59261df1d21b11e51ac6edac0e2e77
zsdasdsad <au30286@gmail.com>
2024-11-01 11:36:24 +0500

    add: login modal window

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,26 +1,11 @@
-import styles from './MainHeader.module.css'
-import Link from "next/link";
+import MainHeader from './header'
 
 export default function Home() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.leftContainer}>
-                <div className={styles.logoContainer}>
-                    <img src='../assets/images.jpg' alt="Pinterest Logo" className={styles.logo}/>
-                </div>
-                <div className={styles.leftNav}>
-                    <Link href="/" className={styles.navLink}>Watch</Link>
-                    <Link href="/" className={styles.navLink}>Explore</Link>
-                </div>
-            </div>
 
-            <nav className={styles.rightNav}>
-                <Link href="/" className={styles.navLink}>About</Link>
-                <Link href="/" className={styles.navLink}>Buisness</Link>
-                <Link href="/" className={styles.navLink}>Blog</Link>
-                <Link href='/' className={styles.loginButton}>Log in</Link>
-                <Link href='/' className={styles.signupButton}>Sign up</Link>
-            </nav>
-        </header>
-    );
+    return (
+        <div>
+            <MainHeader />
+        </div>
+    )
+
 };
